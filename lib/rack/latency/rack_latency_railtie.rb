2cba564ef5c81d557c3ba835ad6d08bc4652dc27
silasb <silas.baronda@gmail.com>
2014-12-18 14:46:40 -0500

    Run reports only when server is defined

--- lib/rack/latency/rack_latency_railtie.rb
+++ lib/rack/latency/rack_latency_railtie.rb
@@ -1,8 +1,10 @@
+require 'pry'
 module Rack
   module Latency
     class RackLatencyRailtie < Rails::Railtie
       initializer "rack_latency_railtie.configure_rails_initialization" do |app|
-        app.middleware.use Rack::Latency::Reporter
+        # only run rack-latency if we are running a server.
+        app.middleware.use Rack::Latency::Reporter if Rails.const_defined? 'Server'
       end
     end
   end
