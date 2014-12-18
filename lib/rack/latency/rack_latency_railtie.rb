148f51723dfb3ad0a7fa1fb652e7929274eaec41
silasb <silas.baronda@gmail.com>
2014-12-18 15:54:07 -0500

    Wrapping this?

--- lib/rack/latency/rack_latency_railtie.rb
+++ lib/rack/latency/rack_latency_railtie.rb
@@ -4,7 +4,7 @@ module Rack
     class RackLatencyRailtie < Rails::Railtie
       initializer "rack_latency_railtie.configure_rails_initialization" do |app|
         # only run rack-latency if we are running a server.
-        app.middleware.use Rack::Latency::Reporter if Rails.const_defined? 'Server'
+        app.middleware.use Rack::Latency::Reporter if Rails.const_defined?('Server')
       end
     end
   end
