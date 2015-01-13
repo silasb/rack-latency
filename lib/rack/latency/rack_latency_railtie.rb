dca89afd49cc278e9a04cb9f145935307cd045c9
silasb <silas.baronda@gmail.com>
2015-01-13 14:25:02 -0500

    Do no start when sidekiq is running

--- lib/rack/latency/rack_latency_railtie.rb
+++ lib/rack/latency/rack_latency_railtie.rb
@@ -4,7 +4,7 @@ module Rack
     class RackLatencyRailtie < Rails::Railtie
       initializer "rack_latency_railtie.configure_rails_initialization" do |app|
         # only run rack-latency if we are running a server.
-        app.middleware.use Rack::Latency::Reporter if !Rails.const_defined?('Console') && !($0 =~ /rake$/) && !Rails.env.test?
+        app.middleware.use Rack::Latency::Reporter if !Rails.const_defined?('Console') && !($0 =~ /rake$/) && !($0 =~ /sidekiq$/) && !Rails.env.test?
       end
     end
   end
